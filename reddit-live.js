0cab4f79d4898673d0a9e80f8cef875fff018a82
Jeremy Roberts <jeremyroberts0@gmail.com>
2014-11-02 16:34:40 -0800

    progress on prototype

--- reddit-live.js
+++ reddit-live.js
@@ -1,7 +1,17 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to reddit-live.";
-  };
+
+    Tracker.autorun(function() {
+        Meteor.call('refreshPosts', function(posts){
+            Template.posts.subreddit = function() {
+                return '!frontpage';
+            };
+            Template.posts.posts = function() {
+                return posts;
+            };
+        });
+
+    });
+
 
   Template.hello.events({
     'click input': function () {
@@ -16,4 +26,40 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+    var postsBySubreddit = { };
+    Meteor.methods({
+        refreshPosts:function(subreddit) {
+            subreddit = subreddit || '!frontpage';
+            return postsBySubreddit[subreddit] || [ ];
+        }
+    });
+    function getPostsFromReddit(options) {
+        options = options || { };
+        var url = 'https://www.reddit.com/';
+        if (options.subreddit) {
+            url += subreddit;
+        }
+        url += '/new';
+        url += options.format || '.json';
+        HTTP.call('GET', url, function(error, result) {
+            if (!error) {
+                if (options.callback) {
+                    var posts = result.data.children.map(function(rawPost) {
+                        return post.data;
+                    });
+                    options.callbacK(posts);
+                }
+            } else {
+                if (options.failure) {
+                    options.failure(error);
+                }
+            }
+        });
+    }
+
+    getPostsFromReddit({
+        callback:function(posts) {
+            postsBySubreddit['!frontpage'] = posts;
+        }
+    });
 }
